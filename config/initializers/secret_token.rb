47e19f99983c2dc4dcc0525552132b637f233713
John Perry <jgperry@gmail.com>
2013-02-05 17:06:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Legitracker2::Application.config.secret_token = '08ce8e2a668a3c3860d5981415a77aeb8e20169c0ce1adc5484d72bc549595bcdfd14cefdda3ed8a897ca1efcee095fa98c76b178bebb0df57ea0a1d77807c53'
